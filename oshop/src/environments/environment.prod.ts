f0cdb4946aa940af60e4dd45cb0efd62d539d8f3
Marcus McKinley <marcusmckinley@Marcuss-MacBook-Pro.local>
2018-07-25 01:34:46 -0400

    app befor deployment

--- oshop/src/environments/environment.prod.ts
+++ oshop/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAbhrahZPHXBNDxKVG6JF_2SChjqyuD-0c",
+    authDomain: "oshop-258e3.firebaseapp.com",
+    databaseURL: "https://oshop-258e3.firebaseio.com",
+    projectId: "oshop-258e3",
+    storageBucket: "oshop-258e3.appspot.com",
+    messagingSenderId: "252482701715"
+  }
 };
--- oshop/src/environments/environment.ts
+++ oshop/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAbhrahZPHXBNDxKVG6JF_2SChjqyuD-0c",
+    authDomain: "oshop-258e3.firebaseapp.com",
+    databaseURL: "https://oshop-258e3.firebaseio.com",
+    projectId: "oshop-258e3",
+    storageBucket: "oshop-258e3.appspot.com",
+    messagingSenderId: "252482701715"
+  }
 };
 
 /*
