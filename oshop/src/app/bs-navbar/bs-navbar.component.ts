21ccbd0fe52eb19632cd26cc1fd4d37227860457
Marcus McKinley <marcusmckinley@Marcuss-MacBook-Pro.local>
2018-07-26 16:57:14 -0400

    added authService for abstraction

--- oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,5 @@
-import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
-import * as firebase from 'firebase';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'bs-navbar',
@@ -9,13 +7,11 @@ import * as firebase from 'firebase';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user$: Observable<firebase.User>;
-  constructor(private afAuth: AngularFireAuth) { 
-    this.user$ = afAuth.authState;
+  constructor(public auth: AuthService) {
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 
 }
