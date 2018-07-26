d51de2676f50b657f71748552a969cea33faf45f
Marcus McKinley <marcusmckinley@Marcuss-MacBook-Pro.local>
2018-07-26 16:43:30 -0400

    Used async pipe to unsubscribe

--- oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,21 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user$: Observable<firebase.User>;
+  constructor(private afAuth: AngularFireAuth) { 
+    this.user$ = afAuth.authState;
+  }
 
-  constructor() { }
-
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
