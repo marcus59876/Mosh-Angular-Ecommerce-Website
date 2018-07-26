27204469821e99a3d41cdbe67d9d0eb3257ae439
Marcus McKinley <marcusmckinley@Marcuss-MacBook-Pro.local>
2018-07-26 17:29:31 -0400

    Set up auth guards so certain pages can only be accessed by logged in users and set where certain redirects go

--- oshop/src/app/auth.service.ts
+++ oshop/src/app/auth.service.ts
@@ -2,18 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '../../node_modules/angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from '../../node_modules/rxjs';
+import { ActivatedRoute } from '../../node_modules/@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user$: Observable<firebase.User>
-  constructor(private afAuth: AngularFireAuth) { 
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
     this.user$ = afAuth.authState;
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('retrurnUrl') || '/';
+    localStorage.setItem('returnUrl',returnUrl);
+
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    
   }
 
   logout() {
