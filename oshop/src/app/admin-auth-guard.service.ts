c61fad0dd5a3191c15d466a8b66ecff61d6292c9
Marcus McKinley <marcusmckinley@Marcuss-MacBook-Pro.local>
2018-07-26 20:22:27 -0400

    Created admin router guards and hid admin links based of admin status of user

---
+++ oshop/src/app/admin-auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '../../node_modules/@angular/router';
+import { AuthService } from './auth.service';
+import { UserService } from './user.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+
+//import { Observable } from '../../node_modules/rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAuthGuard implements CanActivate{
+
+  constructor(private auth: AuthService, private userService: UserService) { }
+
+  canActivate(): Observable<boolean> {
+    return this.auth.appUser$
+      .map(appUser => appUser.isAdmin);
+  
+  }
+}
--- oshop/src/app/auth.service.ts
+++ oshop/src/app/auth.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AppUser } from './models/app-user';
 import { AngularFireAuth } from '../../node_modules/angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from '../../node_modules/rxjs';
 import { ActivatedRoute } from '../../node_modules/@angular/router';
+import { UserService } from './user.service';
+import 'rxjs/add/operator/switchMap';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user$: Observable<firebase.User>
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
+  constructor(private userService: UserService, private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
     this.user$ = afAuth.authState;
   }
 
@@ -23,5 +27,10 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$() : Observable<AppUser> {
+    return this.user$
+      .switchMap(user => this.userService.get(user.uid).valueChanges())
   }
 }
