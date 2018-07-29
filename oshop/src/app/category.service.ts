d2d7b20e91847e451b0a7279bcf579f998ebfd73
Marcus McKinley <marcusmckinley@Marcuss-MacBook-Pro.local>
2018-07-28 20:18:27 -0400

    populated the categories dropdown menu on admin manage products page

---
+++ oshop/src/app/category.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '../../node_modules/angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getCategories() {
+    return this.db
+    .list('/categories', ref => ref.orderByChild('name'))
+    .valueChanges();
+  }
+}
